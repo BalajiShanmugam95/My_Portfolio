54bae43e1fd42a737a1c540f87d8918ac307fc06
Balaji S <balaji6995@gmail.com>
2022-07-01 17:38:59 +0530

    scroll animation and about section update

--- js/needed-actions.js
+++ js/needed-actions.js
@@ -1,25 +1,26 @@
 (function() {
-var isMobile;
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    isMobile = true;
-}
+  AOS.init();
+  var isMobile;
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      isMobile = true;
+  }
 
-var navPos = $('nav').position().top;
-var lastPos = 0;
-$(window).on('scroll', function() {
-    var pos = $(window).scrollTop();
-    var pos2 = pos + 50;
-    var scrollBottom = pos + $(window).height();
+  var navPos = $('nav').position().top;
+  var lastPos = 0;
+  $(window).on('scroll', function() {
+      var pos = $(window).scrollTop();
+      var pos2 = pos + 50;
+      var scrollBottom = pos + $(window).height();
 
-    if (!isMobile) {
-      if (pos >= navPos + $('nav').height() && lastPos < pos) {
-        $('nav').addClass('fixed');
+      if (!isMobile) {
+        if (pos >= navPos + $('nav').height() && lastPos < pos) {
+          $('nav').addClass('fixed');
+        }
+        if (pos < navPos && lastPos > pos) {
+          $('nav').removeClass('fixed');
+        }
+        lastPos = pos;
       }
-      if (pos < navPos && lastPos > pos) {
-        $('nav').removeClass('fixed');
-      }
-      lastPos = pos;
-    }
-})
+  })
 
 })();
